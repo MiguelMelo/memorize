362ce5eff0f25282130b1ddaab48b67e25bd58f0
Carlos Silva <miguel21.melo21@gmail.com>
2023-09-10 14:07:11 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Carlos Silva on 10/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
